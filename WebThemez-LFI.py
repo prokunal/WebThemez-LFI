4d696483bb673f46115f890fca2828aa8b95bd64
Kunal Kumar <k9801263933@gmail.com>
2023-07-16 17:38:42 +0530

    Update WebThemez-LFI.py

--- WebThemez-LFI.py
+++ WebThemez-LFI.py
@@ -13,7 +13,7 @@ while True:
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Referer': 'http://{ip}/index.php?page=home.html'.format(ip),
+        'Referer': 'http://{}/index.php?page=home.html'.format(ip),
         # 'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en-US,en;q=0.9',
         'Connection': 'close',
